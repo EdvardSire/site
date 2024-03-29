0e2b2a4ca3eabb5d08c35525dd5923cf2e25e4ec
EdvardSire <mail@edvardsire.com>
2024-03-30 00:18:38 +0100

    feat: get and remove dotfiles

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,15 @@ commit_hash=$(git rev-parse --short HEAD)
 
 # Print the first 7 characters of the commit hash
 sed -i "s|<footer>commit here</footer>|<footer>$commit_hash</footer>|" exported/index.html
+
+# Dotfiles
+GET=exported/get-dotfiles.sh
+curl -sS https://raw.githubusercontent.com/EdvardSire/dotfiles/main/Dockerfile | awk 'NR > 4 {sub(/^RUN /,"",$0); print}' > $GET
+sed -i 's/\bapt\b/sudo &/g; s/\bmake\b/sudo &/g' $GET
+echo -e '\necho "Remember to :PackerSync"' >> $GET
+
+REMOVE=exported/remove-dotfiles.sh
+curl -sS https://raw.githubusercontent.com/EdvardSire/dotfiles/main/remove-dotfiles.sh > $REMOVE
+
+
+
